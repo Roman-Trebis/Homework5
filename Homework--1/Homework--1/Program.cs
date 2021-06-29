7e0bc1eb40dbf0f9db3ec1b3a1f1d212d2b2cd0b
Roman <roman.trebis77@bk.ru>
2021-06-29 13:13:03 +0300

    1

---
+++ Homework--1/Homework--1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1. Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+            Console.WriteLine(GetFullName("Иван", "Иваныч", "Иванов"));
+            Console.WriteLine(GetFullName("Пётр", "Петрович", "Петров"));
+            Console.WriteLine(GetFullName("Сергей", "Сергеевич", "Сергеев"));
+
+            Console.ReadKey();
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{patronymic} {firstName} {lastName}";
+        }
+    }
+}
