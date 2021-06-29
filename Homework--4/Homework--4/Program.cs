e2f453e5f9e58c01adc1db473ce45b71b6182e5b
Roman <roman.trebis77@bk.ru>
2021-06-29 13:13:58 +0300

    1

---
+++ Homework--4/Homework--4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 4. Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом.
+            Console.Write("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Число Фибоначчи: " + Fibonachi(n));
+            Console.ReadKey();
+        }
+
+        static int Fibonachi(int n)
+        {
+            return Fibonachi(n, out _);
+        }
+
+        static int Fibonachi(int n, out int f1)
+        {
+            f1 = 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int f2;// Число Фибоначчи n - 2
+                f1 = Fibonachi(n - 1, out f2); // Число Фибоначчи n - 1
+                return f1 + f2;
+            }
+        }
+    }
+}
